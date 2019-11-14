c451e3d49499c3f1068c640ca9046f646bd5d35c
goyalion17 <goyalion17@gmail.com>
2019-11-14 18:30:44 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
-    <div className="App">
-      CODEIS
-      <Header />
-      <Technologies />
+    <div className="app-wrapper">
+     <Header />
+     <Nav />
+     <Profile />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <header className="header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpTbH4W80IM8DSRP0knztdd8uqU8f9dNvdU_5S53NBi59nCWoz&s"/>
+    </header>
+    )
+}
+
+export default Header;
