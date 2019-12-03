ab69dd6b1d1b5c230422dc1503b2f935210dfae7
goyalion17 <goyalion17@gmail.com>
2019-12-04 01:45:20 +0200

    cookie, login в теории, auth/me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from 'react-router-dom'
 
-const Header = () => {
+const Header = (props) => {
     return (
     <header className={s.header}>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpTbH4W80IM8DSRP0knztdd8uqU8f9dNvdU_5S53NBi59nCWoz&s" alt='logo' />
+        <div className={s.loginBlock}>
+            { props.isAuth ? props.login : <NavLink to='/login'>Login</NavLink> }
+        </div>
     </header>
     )
 }
--- src/components/redux/redux-store.js
+++ src/components/redux/redux-store.js
@@ -3,12 +3,14 @@ import profileReducer from './profilePage-reducer';
 import dialogsReducer from './dialogsPage-reducer';
 import sidebarReducer from './sidebar-reducer';
 import usersReducer from './users-reducer';
+import authReducer from './auth-reducer';
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 });
 
 let store = createStore(reducers);
