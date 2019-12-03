0428f3a6b07a7827003525d18e9b9cbfce604d07
goyalion17 <goyalion17@gmail.com>
2019-12-03 18:31:53 +0200

    withRouter, props.match.params

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -22,7 +22,7 @@ let Users = (props) => {
             props.users.map(u => <div key={u.id} >
                 <span>
                     <div>
-                        <NavLink to={'/profile' + u.id}>
+                        <NavLink to={'/profile/' + u.id}>
                             <img src={ u.photos.small != null ? u.photos.small : userPhoto } className={styles.photo} />
                         </NavLink>
                     </div>
