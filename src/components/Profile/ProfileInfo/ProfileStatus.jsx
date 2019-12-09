386d39bd2a9f7b20052b84456089049833a1b4a4
goyalion17 <goyalion17@gmail.com>
2019-12-09 17:02:28 +0200

    обновляем create-react-app

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -3,34 +3,35 @@ import s from './ProfileInfo.module.css';
 
 class ProfileStatus extends React.Component {
     state = {
-        editMode: false
+        editMode: false,
+        title: 'Ку'
     }
 
-    activateEditMode () {
+    activateEditMode = () => {
+        console.log('this:', this);
         this.setState({
             editMode: true
         });
     }
 
-    deactivateEditMode () {
+    deactivateEditMode = () => {
         this.setState({
             editMode: false
         });
     }
-
 
     render() {
         return (
             <div>
                 {!this.state.editMode &&
                     <div>
-                        <span onDoubleClick={ this.activateEditMode.bind(this) } >{this.props.status}</span>
+                        <span onDoubleClick={ this.activateEditMode } >{this.props.status}</span>
                     </div>
                 }
         
                 {this.state.editMode &&
                     <div>                    
-                        <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this) } value={this.props.status}/ >
+                        <input autoFocus={true} onBlur={this.deactivateEditMode } value={this.props.status}/ >
                     </div> 
                 }               
             </div>
