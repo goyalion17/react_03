62d32a54a995f112be63c0afe1deed1af6552482
goyalion17 <goyalion17@gmail.com>
2019-12-20 18:18:13 +0200

    Updates

---
+++ precache-manifest.0fd9db8856f58d0bcc6cc753e1843845.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a26c971024de1c2287c9fedcdea5a3",
+    "url": "/react_03/index.html"
+  },
+  {
+    "revision": "61c80f77fc53b28094d3",
+    "url": "/react_03/static/css/3.e48f9290.chunk.css"
+  },
+  {
+    "revision": "eac4026bc24ca67c670a",
+    "url": "/react_03/static/css/4.19a76d65.chunk.css"
+  },
+  {
+    "revision": "6fc2fc4aa3621025fc3b",
+    "url": "/react_03/static/css/main.fe9e50fe.chunk.css"
+  },
+  {
+    "revision": "6d3640da336d5b20e024",
+    "url": "/react_03/static/js/2.4c141886.chunk.js"
+  },
+  {
+    "revision": "aff522dbe087deaeea306a186a35a577",
+    "url": "/react_03/static/js/2.4c141886.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61c80f77fc53b28094d3",
+    "url": "/react_03/static/js/3.72467991.chunk.js"
+  },
+  {
+    "revision": "eac4026bc24ca67c670a",
+    "url": "/react_03/static/js/4.7ed7d493.chunk.js"
+  },
+  {
+    "revision": "6fc2fc4aa3621025fc3b",
+    "url": "/react_03/static/js/main.f8364915.chunk.js"
+  },
+  {
+    "revision": "fc36cf0091d8cc571969",
+    "url": "/react_03/static/js/runtime-main.5a4ce89e.js"
+  },
+  {
+    "revision": "0836a7092d9abdd9cb606306ccee3159",
+    "url": "/react_03/static/media/preloader.0836a709.gif"
+  },
+  {
+    "revision": "4d3d5236299f36c2df17a0b823366eac",
+    "url": "/react_03/static/media/user.4d3d5236.png"
+  }
+]);
